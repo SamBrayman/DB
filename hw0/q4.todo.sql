44409dec9b5463a394f8a1f7bf13ed79409b38d1
Sam Brayman <sbrayma1@jhu.edu>
2017-03-08 21:38:37 -0500

    update q4

--- hw0/q4.todo.sql
+++ hw0/q4.todo.sql
@@ -8,25 +8,125 @@
 
 -- Student SQL code here:
 
-with greatestOrderValueNations as (
-	select nation.name as order_nation_name, sum(orders.totalprice) as order_nation_total_price
-	from nation
-	inner join customer on nation.nationkey = customer.nationkey
-	inner join orders on customer.custkey = orders.custkey
-	group by nation.name--, orders.
+WITH go AS 
+(
+	SELECT nation.name AS order_nation_name, nation.nationkey AS order_nation_nationkey, SUM(orders.totalprice) AS order_nation_total_price, orders.orderkey AS order_keys
+	FROM nation
+	INNER JOIN customer ON nation.nationkey = customer.nationkey
+	INNER JOIN orders ON customer.custkey = orders.custkey
+	GROUP BY customer.nationkey
+	ORDER BY order_nation_total_price DESC
+	LIMIT 5
+),
+
+nation1 AS (
+	SELECT order_nation_name AS order_nation_name1, order_nation_nationkey AS order_nation_nationkey
+	FROM go
+	LIMIT 1
+),
+
+nation2 AS (
+	SELECT order_nation_name AS order_nation_name2, order_nation_nationkey AS order_nation_nationkey
+        FROM go
+        LIMIT 1 OFFSET 1
+),
+
+nation3 AS (
+	SELECT order_nation_name AS order_nation_name3, order_nation_nationkey AS order_nation_nationkey
+        FROM go
+        LIMIT 1 OFFSET 2
+),
+
+nation4 AS (
+	SELECT order_nation_name AS order_nation_name4, order_nation_nationkey AS order_nation_nationkey
+        FROM go
+        LIMIT 1 OFFSET 3
+),
+
+nation5 AS (
+	SELECT order_nation_name AS order_nation_name5, order_nation_nationkey AS order_nation_nationkey
+        FROM go
+        LIMIT 1 OFFSET 4
+),
+
+all1 AS (
+       	SELECT order_nation_name1 AS order_nation_name1, nation.name AS nation_names1, SUM(orders.totalprice) AS tot_price1
+	FROM nation1, nation
+	INNER JOIN customer ON order_nation_nationkey = customer.nationkey
+        INNER JOIN orders ON customer.custkey = orders.custkey
+        INNER JOIN lineitem ON orders.orderkey = lineitem.orderkey
+        INNER JOIN supplier ON lineitem.suppkey = supplier.suppkey
+        WHERE supplier.nationkey = nation.nationkey
+        GROUP BY customer.nationkey, supplier.nationkey
+        ORDER BY order_nation_name1, SUM(lineitem.extendedprice) DESC
+	LIMIT 5
+),
+
+all2 AS (
+        SELECT order_nation_name2 AS order_nation_name2, nation.name AS nation_names2, SUM(orders.totalprice) AS tot_price2
+        FROM nation2, nation
+        INNER JOIN customer on order_nation_nationkey = customer.nationkey
+        INNER JOIN orders on customer.custkey = orders.custkey
+        INNER JOIN lineitem on orders.orderkey = lineitem.orderkey
+        INNER JOIN supplier on lineitem.suppkey = supplier.suppkey
+        WHERE supplier.nationkey = nation.nationkey
+        GROUP BY customer.nationkey, supplier.nationkey
+        ORDER BY order_nation_name2, sum(lineitem.extendedprice) desc
+        LIMIT 5
+),
+
+all3 AS (
+        SELECT order_nation_name3 AS order_nation_name3, nation.name AS nation_names3, SUM(orders.totalprice) AS tot_price3
+        FROM nation3, nation
+        INNER JOIN customer ON order_nation_nationkey = customer.nationkey
+        INNER JOIN orders ON customer.custkey = orders.custkey
+        INNER JOIN lineitem ON orders.orderkey = lineitem.orderkey
+        INNER JOIN supplier ON lineitem.suppkey = supplier.suppkey
+        WHERE supplier.nationkey = nation.nationkey
+        GROUP BY customer.nationkey, supplier.nationkey
+        ORDER BY order_nation_name3, SUM(lineitem.extendedprice) DESC
+        LIMIT 5
+),
+
+all4 AS (
+        SELECT order_nation_name4 AS order_nation_name4, nation.name AS nation_names4, SUM(orders.totalprice) AS tot_price4
+        FROM nation4, nation
+        INNER JOIN customer ON order_nation_nationkey = customer.nationkey
+        INNER JOIN orders ON customer.custkey = orders.custkey
+        INNER JOIN lineitem ON orders.orderkey = lineitem.orderkey
+        INNER JOIN supplier ON lineitem.suppkey = supplier.suppkey
+        WHERE supplier.nationkey = nation.nationkey
+        GROUP BY customer.nationkey, supplier.nationkey
+        ORDER BY order_nation_name4, SUM(lineitem.extendedprice) DESC
+        LIMIT 5
+),
+
+all5 AS (
+        SELECT order_nation_name5 AS order_nation_name5, nation.name AS nation_names5, SUM(orders.totalprice) AS tot_price5
+        FROM nation5, nation
+        INNER JOIN customer ON order_nation_nationkey = customer.nationkey
+        INNER JOIN orders ON customer.custkey = orders.custkey
+        INNER JOIN lineitem ON orders.orderkey = lineitem.orderkey
+        INNER JOIN supplier ON lineitem.suppkey = supplier.suppkey
+        WHERE supplier.nationkey = nation.nationkey
+        GROUP BY customer.nationkey, supplier.nationkey
+        ORDER BY order_nation_name5, SUM(lineitem.extendedprice) DESC
+        LIMIT 5
 )
---with top5Nations as (
-	select *
-	from greatestOrderValueNations
-	order by order_nation_total_price desc
-	limit 5
---)
-
---	select order_nation_name, supplier_nation_name, 
 
 
-
-
-
-
+SELECT order_nation_name1, nation_names1, tot_price1
+FROM all1
+UNION
+SELECT order_nation_name2, nation_names2, tot_price2
+FROM all2
+UNION
+SELECT order_nation_name3, nation_names3, tot_price3
+FROM all3
+UNION
+SELECT order_nation_name4, nation_names4, tot_price4
+FROM all4
+UNION
+SELECT order_nation_name5, nation_names5, tot_price5
+FROM all5
 ;
